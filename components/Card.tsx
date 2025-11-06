4c956c1e7ccb976cf4372de369d66d7003e9e27b
shubh ujala <shubhujala.code@gmail.com>
2025-11-06 10:56:17 +0530

    formatting added to the tags

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,22 +1,33 @@
-import Image from 'next/image'
-import React from 'react'
+import Image from "next/image";
+import React from "react";
 
-interface cardProps{
-    title:string,
-    description: string,
-    img : string,
-    width: number,
-    height:number
+interface cardProps {
+  title: string;
+  description: string;
+  img: string;
+  width: number;
+  height: number;
 }
 
-function Card(props:cardProps) {
+function Card(props: cardProps) {
   return (
-    <div className='p-5 bg-white border-white rounded-2xl shadow-2xl max-w-2xl w-[350px] h-[470px] transition-all duration-200 hover:scale-105 cursor-pointer'>
-        <div className='font-primary text-2xl font-semibold tracking-tighter '>{props.title}</div>
-        <div className='text-md mt-2 font-medium max-w-60'>{props.description}</div>
-        <div className='mt-10 flex justify-center'><Image width={props.width} height={props.height} src={`/${props.img}`} alt='image comes here'/></div>
+    <div className="p-5 bg-white border-white rounded-2xl shadow-2xl max-w-2xl w-[350px] h-[470px] transition-all duration-200 hover:scale-105 cursor-pointer">
+      <div className="font-primary text-2xl font-semibold tracking-tighter ">
+        {props.title}
+      </div>
+      <div className="text-md mt-2 font-medium max-w-60">
+        {props.description}
+      </div>
+      <div className="mt-10 flex justify-center">
+        <Image
+          width={props.width}
+          height={props.height}
+          src={`/${props.img}`}
+          alt="image comes here"
+        />
+      </div>
     </div>
-  )
+  );
 }
 
-export default Card
+export default Card;
