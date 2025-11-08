e6a0bc90bcb2824a574aa77b8e665b6d8617bb59
shubh ujala <shubhujala.code@gmail.com>
2025-11-08 18:56:06 +0530

    added tasks

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from "next/image";
 
 export default function Home() {
   return <>
-    <div className="bg-red-400 text-black">Home page!</div> 
+    <div className="bg-red-400 text-black">Home page!! have to build it! form scratch</div> 
   </>
 }
