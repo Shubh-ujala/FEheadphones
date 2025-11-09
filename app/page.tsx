c5f87a6b2473e0c61ce3445987cfdce6aaf19e86
shubh ujala <shubhujala.code@gmail.com>
2025-11-09 19:02:43 +0530

    home page desgin

--- app/page.tsx
+++ app/page.tsx
@@ -4,8 +4,8 @@ import Image from "next/image";
 export default function Home() {
   return <>
     <div className=" text-black max-w-7xl m-auto h-150 flex justify-evenly ">
-      <div className="pt-[15%] max-w-xl">
-        <div className="tracking-tighter font-bold text-5xl">
+      <div className="pt-[15%] max-w-3xl">
+        <div className="tracking-tighter font-bold text-6xl">
           Explore. Experience. Elevate Your Sound.
         </div>
         <p>Discover top headphones, earbuds, and speakers — all in one place.</p>
