52e6b4fa939d38c4c7f5b2d4f85e1751a1ace8e1
shubh ujala <shubhujala.code@gmail.com>
2025-11-09 19:20:17 +0530

    imported hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,14 @@
+import Hero from "@/components/HomePage/Hero";
 import Marque from "@/components/Marque";
 import Image from "next/image";
 
 export default function Home() {
-  return <>
-    <div className=" text-black max-w-7xl m-auto h-150 flex justify-evenly ">
-      <div className="pt-[15%] max-w-3xl">
-        <div className="tracking-tighter font-bold text-6xl">
-          Explore. Experience. Elevate Your Sound.
-        </div>
-        <p>Discover top headphones, earbuds, and speakers — all in one place.</p>
+  return (
+    <>
+      {/* hero section comes here */}
+      <div className="max-w-7xl m-auto">
+        <Hero />
       </div>
-      <div className="flex flex-col justify-center ">
-        <Marque/>
-      </div>
-    </div> 
-  </>
+    </>
+  );
 }
