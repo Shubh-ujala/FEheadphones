3696391e58b173207ab6f94eb122368be9a4ee53
shubh ujala <shubhujala.code@gmail.com>
2025-11-09 19:01:35 +0530

    solved the bottom scroll issue

--- components/Marque.tsx
+++ components/Marque.tsx
@@ -17,21 +17,25 @@ const ImgUrl = [
   {
       id: 4,
       url: '/headphoneMarquee.png'
+  },
+  {
+      id: 5,
+      url: '/moreMarqueeImage.png'
   }
 ];
 
 
 function Marque() {
   return (
-    <div className="h-96 w-[500px]  mr-5  flex pt-30 justify-center mask-r-from-80% mask-l-from-80% max-w-3xl">
-      <div className="flex gap-8 animate-marquee">
+    <div className="relative h-96 w-[500px]  mr-5  flex pt-30 justify-center mask-r-from-80% mask-l-from-80% max-w-3xl overflow-hidden">
+      <div className="flex gap-8 animate-marquee absolute">
             {ImgUrl.map((ele,key)=>(
             <div key={key} className="">
                 <Image width={80} height={80} src={ele.url} alt='image' className="rounded-full "/>
             </div>
         ))}
-        
       </div>
+      
     </div>
   );
 }
