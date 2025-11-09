6550b63ba67e139649fcc16779dffe3e02d6d3d1
shubh ujala <shubhujala.code@gmail.com>
2025-11-10 00:01:12 +0530

    some marginal changes applied

--- components/HomePage/Hero.tsx
+++ components/HomePage/Hero.tsx
@@ -1,20 +1,35 @@
-import React from 'react'
-import Marque from '../Marque'
-
+import React from "react";
+import Marque from "../Marque";
+import { MdOutlineArrowRightAlt } from "react-icons/md";
+import Link from "next/link";
 function Hero() {
   return (
-    <div className=" text-black max-w-7xl m-auto h-150 flex justify-evenly ">
-      <div className="pt-[15%] max-w-3xl">
-        <div className="tracking-tighter font-bold text-6xl leading-17">
-        Explore. Experience. Elevate Your Sound.
+    <>
+      <div className=" text-black max-w-7xl m-auto flex justify-evenly ">
+        <div className="pt-[8%] max-w-3xl">
+          <div className="tracking-tighter font-bold text-6xl leading-17 text-center">
+            Explore. Experience. Elevate Your Sound.
+          </div>
+          <p className="text-sm font-normal text-center max-w-3xl">
+            Discover premium headphones, earbuds, and speakers designed to
+            deliver pure sound, comfort, and style. Experience music the way
+            it’s meant to be — clear, powerful, and immersive.
+          </p>
         </div>
-        <p className="text-sm font-normal ml-2 max-w-3xl">Discover premium headphones, earbuds, and speakers designed to deliver pure sound, comfort, and style. Experience music the way it’s meant to be — clear, powerful, and immersive.</p>
       </div>
-      <div className="flex flex-col justify-center ">
-        <Marque/>
+      <div className="flex justify-center max-w-4xl m-auto ">
+        <Marque />
       </div>
-    </div> 
-  )
+
+      <div className="flex justify-center items-center mb-4"> 
+            <Link href={'/products'}>
+            <div className="">Explore more</div>
+            </Link>
+            <Link href={'/products'} className="mt-1"><MdOutlineArrowRightAlt size={25}/>
+            </Link>
+      </div>
+    </>
+  );
 }
 
-export default Hero
+export default Hero;
--- components/Marque.tsx
+++ components/Marque.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 
+
 const ImgUrl = [
   {
     id: 1,
@@ -12,7 +13,7 @@ const ImgUrl = [
 },
 {
     id: 3,
-    url: '/speakerMarquee1.jpg'
+    url: '/speakers.png'
   },
   {
       id: 4,
@@ -20,19 +21,23 @@ const ImgUrl = [
   },
   {
       id: 5,
-      url: '/moreMarqueeImage.png'
+      url: '/headphoneBrown.png'
+  },
+  {
+      id: 6,
+      url: '/boatMarquee.png'
   }
 ];
 
 
 function Marque() {
   return (
-    <div className="relative h-96 w-[500px]  mr-5  flex pt-30 justify-center mask-r-from-80% mask-l-from-80% max-w-3xl overflow-hidden">
-      <div className="flex gap-8 animate-marquee absolute">
+    <div className="relative p-5 w-full  mr-5  flex pt-30 justify-center max-w-3xl overflow-hidden  mask-l-from-80% mask-r-from-80%">
+      <div className="flex gap-8 animate-marquee ">
             {ImgUrl.map((ele,key)=>(
-            <div key={key} className="">
-                <Image width={80} height={80} src={ele.url} alt='image' className="rounded-full "/>
-            </div>
+            // <div key={key} className="">
+                <Image key={key} width={100} height={100} src={ele.url} alt='image' className="mx-4 object-contain" />
+            // </div>
         ))}
       </div>
       
