417a32e9f3049f50c682cb469df3d028640e2525
shubh ujala <shubhujala.code@gmail.com>
2025-11-10 00:00:55 +0530

    added footer component

---
+++ components/HomePage/Footer.tsx
@@ -0,0 +1,13 @@
+import Link from 'next/link'
+import React from 'react'
+
+function Footer() {
+  return (
+    <div className='w-full bg-neutral-900 text-sm text-white p-5 flex justify-between font-mono'>
+         <Link href={'https://x.com/shubhXc0de'} className='pl-2 '> Shubh ujala</Link>
+         <Link href={'https://x.com/shubhXc0de'} className='pl-2 '> Shubh ujala</Link>
+    </div>
+  )
+}
+
+export default Footer
