80b2910ecfc6705a232e56c38244c1afcd1765c9
shubh ujala <shubhujala.code@gmail.com>
2025-11-13 02:18:31 +0530

    created generic card component

--- components/ProductsPage/ProductCard.tsx
+++ components/ProductsPage/ProductCard.tsx
@@ -1,3 +1,5 @@
+import { url } from 'inspector';
+import Image from 'next/image';
 import React from 'react'
 
 interface card{
@@ -13,10 +15,20 @@ interface jsonData{
 
 
 function ProductCard(props:jsonData) {
-  console.log(props);
+  // console.log(props.items);
+  const content = props.items;
   return (
-    <div>
-       
+    <div className='flex justify-evenly flex-wrap gap-10'>
+          {content.map((ele,key)=>(
+             <div key={key} className='w-70 h-90 rounded-xl transition-all ease-in-out hover:scale-105 bg-neutral-300/50 p-5 cursor-pointer hover:shadow-xl/20'>
+                  <div className={`w-50 h-50 object-contain bg-no-repeat`}
+                  style={{ backgroundImage: `url(${ele.img})` }}>
+                  </div>
+                  <div className='text-xl font-semibold'>{ele.brand}</div>
+                  <div>{ele.name}</div>
+                  <div>{ele.price}</div>
+             </div>
+          ))}
     </div>
   )
 }
