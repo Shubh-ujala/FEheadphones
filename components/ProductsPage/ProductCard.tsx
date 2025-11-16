1a849952cfbedaab244481833ad1254252b14b2e
shubh ujala <shubhujala.code@gmail.com>
2025-11-16 18:34:53 +0530

    updated the product card

--- components/ProductsPage/ProductCard.tsx
+++ components/ProductsPage/ProductCard.tsx
@@ -23,7 +23,7 @@ function ProductCard(props:jsonData) {
     <div className='flex justify-evenly flex-wrap gap-10'>
           {content.map((ele,key)=>(
              <Link key={key} href={`/product/${ele.category}/${ele.id}`}>
-              <div  className='w-70 h-85 rounded-xl transition-all ease-in-out hover:scale-105 bg-neutral-300/50 p-5 cursor-pointer hover:shadow-xl/20 flex justify-center' >
+              <div  className='w-70 h-85 rounded-xl transition-all ease-in-out hover:scale-105 bg-neutral-300/50 p-6 cursor-pointer hover:shadow-xl/20 flex justify-center ' >
                   <div><div className={`w-50 h-50 object-contain bg-center bg-no-repeat `}
                   >
                     <Image
@@ -31,11 +31,11 @@ function ProductCard(props:jsonData) {
                         height={200}
                         alt='image'
                         src={ele.img}
-                        className='rounded-xl'
+                        className='rounded-xl content-center'
                     />
                   </div>
                   <div className='text-xl font-semibold'>{ele.brand}</div>
-                  <div>{ele.name}</div>
+                  <div className=''>{ele.name}</div>
                   <div>{ele.price}</div></div>
              </div>
              </Link>
