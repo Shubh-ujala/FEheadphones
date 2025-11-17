5a9137b8c3cef94e002261a8e08ee7893dd162a3
shubh ujala <shubhujala.code@gmail.com>
2025-11-17 19:21:06 +0530

    fixed the image specifications

--- components/ProductsPage/ProductCard.tsx
+++ components/ProductsPage/ProductCard.tsx
@@ -1,48 +1,45 @@
-import { url } from 'inspector';
-import Image from 'next/image';
-import Link from 'next/link';
-import React from 'react'
+import Image from "next/image";
+import Link from "next/link";
+import React from "react";
 
-interface card{
-    id: string,
-    img : string,
-    images:string[]
-    brand: string,
-    price : string,
-    name: string,
-    category: string
-}
-interface jsonData{
-    items : card[]
+interface Card {
+  id: string;
+  img: string;
+  images: string[];
+  brand: string;
+  price: string;
+  name: string;
+  category: string;
 }
 
+interface JsonData {
+  items: Card[];
+}
 
-function ProductCard(props:jsonData) {
-  // console.log(props.items);
-  const content = props.items;
+function ProductCard({ items }: JsonData) {
   return (
-    <div className='flex justify-evenly flex-wrap gap-10'>
-          {content.map((ele,key)=>(
-             <Link key={key} href={`/product/${ele.category}/${ele.id}`}>
-              <div  className='w-70 h-85 rounded-xl transition-all ease-in-out hover:scale-105 bg-neutral-300/50 p-6 cursor-pointer hover:shadow-xl/20 flex justify-center ' >
-                  <div><div className={`w-50 h-50 object-contain bg-center bg-no-repeat `}
-                  >
-                    <Image
-                        width={200}
-                        height={200}
-                        alt='image'
-                        src={ele.images[0]}
-                        className='rounded-xl content-center'
-                    />
-                  </div>
-                  <div className='text-xl font-semibold'>{ele.brand}</div>
-                  <div >{ele.name}</div>
-                  <div>{ele.price}</div></div>
-             </div>
-             </Link>
-          ))}
+    <div className="flex justify-evenly flex-wrap gap-10">
+      {items.map((ele, key) => (
+        <Link key={key} href={`/product/${ele.category}/${ele.id}`}>
+          <div className="w-72 rounded-xl bg-neutral-300/50 p-6 cursor-pointer hover:scale-105 transition-all hover:shadow-xl">
+        
+            <div className="relative w-full h-56 flex justify-center items-center">
+              <Image
+                src={ele.images[0]}
+                alt={ele.name}
+                fill
+                className="object-contain rounded-xl"
+              />
+            </div>
+
+            <div className="mt-4 text-xl font-semibold">{ele.brand}</div>
+            <div className="text-sm">{ele.name}</div>
+            <div className="font-medium">{ele.price}</div>
+          </div>
+        </Link>
+      ))}
     </div>
-  )
+  );
 }
 
-export default ProductCard
+export default ProductCard;
